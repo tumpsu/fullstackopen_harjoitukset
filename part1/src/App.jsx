35c89f1032e5a4cf04374d4e0a2d3fcf2cc5a104
Tuomas Keskitalo <keskitalotuomas@outlook.com>
2025-09-09 08:06:32 +0300

    JInside compoment, destruct object.

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,7 +1,6 @@
 const Hello = (props) => {
   console.log(props);
-  const name = props.name
-  const age = props.age
+  const { name, age } = props;
   const bornYear = () => new Date().getFullYear() - age;
 
   return (
