26b0ca9486b63a19f320543a93cc21ff2708b8ec
Tuomas Keskitalo <keskitalotuomas@outlook.com>
2025-09-09 08:01:00 +0300

    Added logic to determine when person is born.

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,10 +1,17 @@
 const Hello = (props) => {
-  console.log(props)
+  console.log(props);
+  const bornYear = () => {
+    const yearNow = new Date().getFullYear()
+    return yearNow - props.age
+  }
+
+
   return (
     <div>
       <p>
-        Hello {props.name}, you are {props.age} years old
+        Hello {props.name}, you are {props.age} years old.
       </p>
+      <p>So you were probably born {bornYear()}.</p>
     </div>
   )
 }
