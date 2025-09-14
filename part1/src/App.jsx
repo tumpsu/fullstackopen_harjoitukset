d287636b5ddabf7c4a092aca2ec1694a3423ddba
Tuomas Keskitalo <keskitalotuomas@outlook.com>
2025-09-14 22:20:00 +0300

    Added counter increment button and event handler.

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,17 +1,21 @@
 import { useState } from 'react';
-
 const App = () => {
-
   const [ counter, setCounter ] = useState(0);
 
 
-  setTimeout(
-    () => setCounter(counter + 1),
-    1000
-  );
-  console.log('rendering...', counter);
+  const handleClick = () => {
+    setCounter(counter + 1);
+    console.log('clicked, counter is ' + counter);
+  }
+
   return (
-    <div>{counter}</div>
+    <div>
+      <div>{counter}</div>
+
+      <button onClick={handleClick}>
+        plus
+      </button>
+    </div>
   )
 }
 
