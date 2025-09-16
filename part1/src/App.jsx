83b7dfc066003d3cda0fab1cc1a6393cfcb982c2
Tuomas Keskitalo <keskitalotuomas@outlook.com>
2025-09-16 19:58:01 +0300

    Added debug code.

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -16,10 +16,20 @@ const Button = (props) => {
 
 const App = () => {
   const [ counter, setCounter ] = useState(0);
+  console.log('rendering with counter value', counter);
 
-  const increaseByOne = () => setCounter(counter + 1);
-  const decreaseByOne = () => setCounter(counter - 1);
-  const setToZero = () => setCounter(0);
+  const increaseByOne = () => {
+    console.log('increasing, value before', counter);
+    setCounter(counter + 1);
+  } 
+  const decreaseByOne = () => {
+    console.log('decreasing, value before', counter);
+    setCounter(counter - 1);
+  }
+  const setToZero = () => {
+    console.log('resetting to zero, value before', counter);
+    setCounter(0);
+  }
 
   
   return (
