5d08e0400afbf06497e04b58765ca6de24a59e17
Tuomas Keskitalo <keskitalotuomas@outlook.com>
2025-09-19 22:19:20 +0300

    Fixed  async counter update pronlem

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -19,17 +19,23 @@ const App = () => {
   const [left, setLeft] = useState(0);
   const [right, setRight] = useState(0);
   const [allClicks, setAll] = useState([]);
-
+  const [total, setTotal] = useState(0);
 
   const handleLeftClick = () => {
     setAll(allClicks.concat('L'));
-    setLeft(left + 1);
+    const updatedLeft = left + 1;
+    console.log('left before', updatedLeft);
+    setLeft(updatedLeft);
+    console.log('left after', left);
+    setTotal(updatedLeft + right);
   }
 
 
   const handleRightClick = () => {
     setAll(allClicks.concat('R'));
-    setRight(right + 1);
+    const updatedRight = right + 1;
+    setRight(updatedRight);
+    setTotal(left + updatedRight);
   }
 
   return (
@@ -41,6 +47,7 @@ const App = () => {
         {right}
 
         <p>{allClicks.join(' ')}</p>
+        <p>total {total}</p>
       </div>
     </div>
   )
