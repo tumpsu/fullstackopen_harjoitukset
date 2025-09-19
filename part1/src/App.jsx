28209572b47167cc21bc6fe1f7fa476481caa5a4
Tuomas Keskitalo <keskitalotuomas@outlook.com>
2025-09-20 00:20:57 +0300

    function retuen another function.

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -3,16 +3,21 @@ import { useState } from 'react';
 const App = (props) => {
   const [value, setValue] = useState(10);
 
-  const handleClick = () => {
-    console.log('clicked the button');
-    setValue(0);
-  }
 
+  const hello = (who) => {
+    const handler = () => {
+      console.log('hello', who);
+    }
+    return handler;
+  }
 
   return (
     <div>
       {value}
-      <button onClick={ handleClick }>button</button>
+
+      <button onClick={hello('world')}>button</button>
+      <button onClick={hello('react')}>button</button>
+      <button onClick={hello('function')}>button</button>
     </div>
   )
 }
