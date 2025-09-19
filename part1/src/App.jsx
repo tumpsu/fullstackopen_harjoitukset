2d8a072655e106c1e2aa21eb09e848a4961e825a
Tuomas Keskitalo <keskitalotuomas@outlook.com>
2025-09-19 23:07:16 +0300

    Event handler is passes as a reference to buttun.

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -3,10 +3,16 @@ import { useState } from 'react';
 const App = (props) => {
   const [value, setValue] = useState(10);
 
+  const handleClick = () => {
+    console.log('clicked the button');
+    setValue(0);
+  }
+
+
   return (
     <div>
       {value}
-      <button onClick={() => setValue(0)}>button</button>
+      <button onClick={ handleClick }>button</button>
     </div>
   )
 }
