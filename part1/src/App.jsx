fd74e7f00446ad130a8f19cb85c5c9aee42c3243
Tuomas Keskitalo <keskitalotuomas@outlook.com>
2025-09-19 21:13:46 +0300

    Added useState.

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -13,36 +13,23 @@ const Button = ({ onClick, text }) => {
     </button>
   )
 }
-
 const App = () => {
-  const [left, setLeft] = useState(0);
-  const [right, setRight] = useState(0);
-
-  const [allClicks, setAll] = useState([]);
-
-
-  const handleLeftClick = () => {
-    setAll(allClicks.concat('L'));
-    setLeft(left + 1);
-  }
-
-  const handleRightClick = () => {
-    setAll(allClicks.concat('R'));
-    setRight(right + 1);
-  }
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
 
   return (
     <div>
       <div>
         {left}
-        <button onClick={handleLeftClick}>left</button>
-        <button onClick={handleRightClick}>right</button>
+        <button onClick={() => setLeft(left + 1)}>
+          left
+        </button>
+        <button onClick={() => setRight(right + 1)}>
+          right
+        </button>
         {right}
-
-        <p>{allClicks.join(' ')}</p>
       </div>
     </div>
   )
 }
-
 export default App;
