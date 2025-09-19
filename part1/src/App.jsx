64866118f0d07e713d47f54ac5bc6e3f3173222a
Tuomas Keskitalo <keskitalotuomas@outlook.com>
2025-09-19 22:10:38 +0300

    Changed spread syntax to used when use left and right button. Added array to remember button clics

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -14,34 +14,33 @@ const Button = ({ onClick, text }) => {
   )
 }
 
+
 const App = () => {
-  const [clicks, setClicks] = useState({
-    left: 0, right: 0
-  })
+  const [left, setLeft] = useState(0);
+  const [right, setRight] = useState(0);
+  const [allClicks, setAll] = useState([]);
+
 
   const handleLeftClick = () => {
-    const newClicks = { 
-      left: clicks.left + 1, 
-      right: clicks.right 
-    }
-    setClicks(newClicks)
+    setAll(allClicks.concat('L'));
+    setLeft(left + 1);
   }
 
+
   const handleRightClick = () => {
-    const newClicks = { 
-      left: clicks.left, 
-      right: clicks.right + 1 
-    }
-    setClicks(newClicks)
+    setAll(allClicks.concat('R'));
+    setRight(right + 1);
   }
 
   return (
     <div>
       <div>
-        {clicks.left}
+        {left}
         <button onClick={handleLeftClick}>left</button>
         <button onClick={handleRightClick}>right</button>
-        {clicks.right}
+        {right}
+
+        <p>{allClicks.join(' ')}</p>
       </div>
     </div>
   )
