5160123c73e6bda447a4ea90c61376912dba14e5
Tuomas Keskitalo <keskitalotuomas@outlook.com>
2025-09-19 22:27:05 +0300

    Use buttom compoment to handle button press events.

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,19 +1,5 @@
 import { useState } from 'react';
 
-const Display = ({ counter }) => {
-  return (
-    <div>{counter}</div>
-  )
-}
-
-const Button = ({ onClick, text }) => {
-  return (
-    <button onClick={onClick}>
-      {text}
-    </button>
-  )
-}
-
 const History = (props) => {
   if (props.allClicks.length === 0) {
     return (
@@ -22,6 +8,7 @@ const History = (props) => {
       </div>
     )
   }
+
   return (
     <div>
       button press history: {props.allClicks.join(' ')}
@@ -29,42 +16,32 @@ const History = (props) => {
   )
 }
 
+
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
+
 const App = () => {
-  const [left, setLeft] = useState(0);
-  const [right, setRight] = useState(0);
-  const [allClicks, setAll] = useState([]);
-  const [total, setTotal] = useState(0);
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
 
   const handleLeftClick = () => {
-    setAll(allClicks.concat('L'));
-    const updatedLeft = left + 1;
-    console.log('left before', updatedLeft);
-    setLeft(updatedLeft);
-    console.log('left after', left);
-    setTotal(updatedLeft + right);
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)
   }
 
-
   const handleRightClick = () => {
-    setAll(allClicks.concat('R'));
-    const updatedRight = right + 1;
-    setRight(updatedRight);
-    setTotal(left + updatedRight);
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
   }
 
   return (
     <div>
-      <div>
-        {left}
-        <button onClick={handleLeftClick}>left</button>
-        <button onClick={handleRightClick}>right</button>
-        {right}
-
-        <p>{allClicks.join(' ')}</p>
-        <History allClicks={allClicks} />
-      </div>
+      {left}
+      <Button onClick={handleLeftClick} text='left' />
+      <Button onClick={handleRightClick} text='right' />
+      {right}
+      <History allClicks={allClicks} />
     </div>
   )
 }
-
 export default App;
