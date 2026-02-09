383ea8bd4c465a9a00c62c7738fbadced4983b68
Tuomas Keskitalo <keskitalotuomas@outlook.com>
2026-02-09 17:48:50 +0200

    Reavt and API cors fixes.

--- part3/notesapi/index.js
+++ part3/notesapi/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 app.use(express.json()); // Use json parser
-
+app.use(cors());
 let notes = [
   {
     id: "1",
@@ -87,7 +88,7 @@ response
 .end();
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
